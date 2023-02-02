8c6530d0f7c516b101d7299d6e51d7affdddeb83
DoOoMB <goworuha.egor@yandex.ru>
2023-02-02 19:58:59 +0300

    Добавлены выриации моделек кучи ресурсов

--- Assets/Scripts/Globals/Resourses/Resourse.cs
+++ Assets/Scripts/Globals/Resourses/Resourse.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Globals.Resourses
 {
@@ -9,8 +11,9 @@ namespace Assets.Scripts.Globals.Resourses
         protected abstract int count { get; set; }
         protected abstract int resLimit { get; }
         public abstract string resName { get; }
-        protected abstract GameObject resCounter { get; }
-        public abstract void updateHUD();
+        public abstract void addCount(int value);
+        public abstract void subCount(int value);
+        public abstract void setCount(int value);
     }
 }
 
